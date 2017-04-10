d4965fb43411dabf827f5657edc1fb501f455679
BrockleyJohn - John Ferguson <github@yourmontage.co.uk>
2017-04-10 13:17:47 +0100

    version 1.0

---
+++ includes/languages/english/modules/content/product_info/cm_pi_products_url.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  Author: John Ferguson @BrockleyJohn john@sewebsites.net
+	
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2017 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_CONTENT_PRODUCT_INFO_PRODUCTS_URL_TITLE', 'Product URL');
+  define('MODULE_CONTENT_PRODUCT_INFO_PRODUCTS_URL_DESCRIPTION', 'Shows the external URL on the Product Info Page.');
+  
+  define('MODULE_CONTENT_PRODUCT_INFO_PRODUCTS_URL_PUBLIC_TITLE', 'Manufacturer link: ');
---
+++ includes/modules/content/product_info/templates/products_url.php
@@ -0,0 +1,8 @@
+<div class="col-sm-<?php echo $content_width; ?> products-url">
+  <ul class="list-group">
+    <li class="list-group-item">
+      <?php echo MODULE_CONTENT_PRODUCT_INFO_PRODUCTS_URL_PUBLIC_TITLE; ?>
+      <a href="//<?php echo $product_info['products_url'] . '" target="_blank">' . $product_info['products_name']; ?></a>
+    </li>
+  </ul>
+</div>
