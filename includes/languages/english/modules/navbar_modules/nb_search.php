11c484a407e8bb9e731be9288018b300396de17d
BrockleyJohn - John Ferguson <github@yourmontage.co.uk>
2018-02-21 18:07:50 +0000

    w-i-p

---
+++ includes/languages/english/modules/navbar_modules/nb_search.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com 
+
+  Copyright (c) 2016 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_NAVBAR_SEARCH_TITLE', 'Search');
+  define('MODULE_NAVBAR_SEARCH_DESCRIPTION', 'Show Search in Navbar.');
+    
